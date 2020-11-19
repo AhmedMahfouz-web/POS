7cf1519d64654cceeed04a0f7acb7dcddd0d66ab
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2020-11-19 12:32:15 +0200

    First Commit

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -1,7 +1,10 @@
 <?php
 
-Route::prefix('dashboard')->name('dashboard.')->group(function() {
+Route::group(['prefix' => LaravelLocalization::setLocale()], function()
+{
+    Route::prefix('dashboard')->name('dashboard.')->group(function() {
 
-    Route::get('/index', 'DashboardController@index')->name('index');
+        Route::get('/index', 'DashboardController@index')->name('index');
 
-});//end of dashboard routes
+    });//end of dashboard routes
+});
